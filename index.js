f796f6d11f11ad45b88239a75d58ddf836bd61d1
张涛 <zhangtao@tsingyun.net>
2019-09-06 07:03:01 +0800

    解决打包输出路径及环境选择暴露

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-console.log("tyBuild test index");
 const prod = require("./lib/build/webpack.prod");
 const dev = require("./lib/build/webpack.dev");
 const mock = require("./lib/build/webpack.mock");
 const proxyDev = require("./lib/build/webpack.proxy.dev");
 const proxyMock = require("./lib/build/webpack.proxy.mock");
 
-module.exports = {
-    prod,
-    dev,
-    mock,
-    proxyDev,
-    proxyMock
+module.exports = function(env){
+    if(env.prod){
+        return prod;
+    }
+    if(env.dev){
+        return dev;
+    }
+    if(env.start){
+        return mock;
+    }
+    if(env.proxyDev){
+        return proxyDev;
+    }
+    if(env.proxyMock){
+        return proxyMock;
+    }
 }
