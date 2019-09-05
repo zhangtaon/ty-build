6f079a70c2826a0b8c542b744fbda97d3f755ed4
张涛 <zhangtao@tsingyun.net>
2019-09-05 19:01:53 +0800

    la

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 console.log("tyBuild test index");
+const prod = require("./lib/build/webpack.prod");
+const dev = require("./lib/build/webpack.dev");
+const mock = require("./lib/build/webpack.mock");
+const proxyDev = require("./lib/build/webpack.proxy.dev");
+const proxyMock = require("./lib/build/webpack.proxy.mock");
+
 module.exports = {
-    tyBuild: "tyBuild test"
+    prod,
+    dev,
+    mock,
+    proxyDev,
+    proxyMock
 }
