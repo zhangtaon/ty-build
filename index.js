4d6f0af9f61b2334a2ab5a4b2fb65888eb4208df
张涛 <zhangtao@tsingyun.net>
2019-10-17 16:51:08 +0800

    增加fat环境支持

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const prod = require("./lib/build/webpack.prod");
 const dev = require("./lib/build/webpack.dev");
+const fat = require("./lib/build/webpack.fat");
 const mock = require("./lib/build/webpack.mock");
 const proxyDev = require("./lib/build/webpack.proxy.dev");
+const proxyFat = require("./lib/build/webpack.proxy.fat");
 const proxyMock = require("./lib/build/webpack.proxy.mock");
 
 module.exports = function(env){
     if(env.prod){
         return prod;
+    }
+    if(env.fat){
+        return fat;
     }
     if(env.dev){
         return dev;
@@ -16,6 +21,9 @@ module.exports = function(env){
     }
     if(env.proxyDev){
         return proxyDev;
+    }
+    if(env.proxyFat){
+        return proxyFat;
     }
     if(env.proxyMock){
         return proxyMock;
